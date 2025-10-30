7a4776ad5a9b5c1a766b7331255bf3597d6d4522
Malte <malte.schuette92@gmail.com>
2025-10-30 16:19:32 +0100

    added featured projects css, added references, startet contact form logic

--- src/app/main-page/contact/contact.ts
+++ src/app/main-page/contact/contact.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
 export class Contact {
 
+  contactData= {
+    name:"",
+    email: "",
+    message: "",
+  }
+
+  onSubmit(ngForm: NgForm) {
+    
+    if(ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    }
+  }
 }
--- src/app/main-page/reference/reference.ts
+++ src/app/main-page/reference/reference.ts
@@ -8,31 +8,50 @@ import { Component } from '@angular/core';
 })
 export class Reference {
 
-references = [
-  {
-    name: 'Rick Astley',
-    comment: `We're no strangers to love
-              You know the rules and so do I
-              A full commitment's what I'm thinkin' of
-              You wouldn't get this from any other guy
-              I just wanna tell you how I'm feeling
-              Gotta make you understand`
-  },
-  {
-    name: 'Rick Astley',
-    comment: `Never gonna give you up, never gonna let you down
-              Never gonna run around and desert you
-              Never gonna make you cry, never gonna say goodbye
-              Never gonna tell a lie and hurt you`
-  },
-  {
-    name: 'Rick Astley',
-    comment: `We've known each other for so long
-              Your heart's been aching, but you're too shy to say it
-              Inside, we both know what's been going on
-              We know the game and we're gonna play it`
+  activeIndex = 1;
+
+  references = [
+    {
+      name: 'Rick Astley',
+      comment: `We're no strangers to love
+                You know the rules and so do I
+                A full commitment's what I'm thinkin' of
+                You wouldn't get this from any other guy
+                I just wanna tell you how I'm feeling
+                Gotta make you understand`
+    },
+    {
+      name: 'Rick Astley',
+      comment: `Never gonna give you up, never gonna let you down
+                Never gonna run around and desert you
+                Never gonna make you cry, never gonna say goodbye
+                Never gonna tell a lie and hurt you`
+    },
+    {
+      name: 'Rick Astley',
+      comment: `We've known each other for so long
+                Your heart's been aching, but you're too shy to say it
+                Inside, we both know what's been going on
+                We know the game and we're gonna play it`
+    },
+    {
+      name: 'Rick Astley',
+      comment: `Test23`
+    }
+  ];
+
+  next() {
+    this.activeIndex = (this.activeIndex + 1) % this.references.length;
   }
-];
 
+  prev() {
+    this.activeIndex = (this.activeIndex - 1 + this.references.length) % this.references.length;
+  }
 
+  visibleReferences() {
+    const len = this.references.length;
+    const left = (this.activeIndex - 1 + len) % len;
+    const right = (this.activeIndex + 1) % len;
+    return [this.references[left], this.references[this.activeIndex], this.references[right]];
+  }
 }
